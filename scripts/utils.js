d3be042ede10ad3c5c612e2da24f370a8d0089bb
Sefinek <contact@sefinek.net>
2024-12-18 23:08:12 +0100

    Po prostu zmiana ścieżki

--- scripts/utils.js
+++ scripts/utils.js
@@ -7,7 +7,7 @@ const blockedResources = new Set(['image', 'icon', 'imageset', 'font', 'media', 
 const blockedDomains = new Set(['server.cpmstar.com', 'pagead2.googlesyndication.com', 'gstatic.com', 'www.gstatic.com', 'doubleclick.net', 'www.youtube.com', 'facebook.com', 'connect.facebook.net', 'twitter.com', 'platform.twitter.com', 'instagram.com', 'cdn.jsdelivr.net', 'adservice.google.com', 'ads.yahoo.com', 'ads.twitter.com', 'ytimg.com', 'google-analytics.com', 'googletagmanager.com', 'googletagservices.com', 'fonts.googleapis.com']);
 
 const createProfileDir = () => {
-	const profilePath = path.join(__dirname, '..', 'chrome', 'profiles', 'raid', Date.now().toString());
+	const profilePath = path.join(__dirname, '..', 'chrome', 'profiles', Date.now().toString());
 	if (!fs.existsSync(profilePath)) fs.mkdirSync(profilePath, { recursive: true });
 	return { path: profilePath };
 };
